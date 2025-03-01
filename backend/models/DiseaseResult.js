95fb6281d561915654ad46a258b088d813266fa2
BarroJohnNems <johnbarro977@gmail.com>
2025-03-01 13:59:49 +0800

    batch

--- backend/models/DiseaseResult.js
+++ backend/models/DiseaseResult.js
@@ -1,14 +1,3 @@
-const mongoose = require("mongoose");
-
-const DiseaseResultSchema = new mongoose.Schema({
-  image_url: { type: String, required: true }, 
-  predicted_disease: { type: String, required: true }, 
-  confidence: { type: Number, required: true }, 
-  location: {
-    latitude: { type: Number, required: false },
-    longitude: { type: Number, required: false }
-  },
-  timestamp: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("DiseaseResult", DiseaseResultSchema);
+version https://git-lfs.github.com/spec/v1
+oid sha256:4acb57799f3a34a968f8ac9ff86fe69f601b5611401b892915f3c5254f0534f6
+size 482
